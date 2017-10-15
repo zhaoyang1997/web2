db620010cb2c29e4237ca53bf3130f6fa2733a04
zhaoyang1997 <m15226510335@163.com>
2017-10-15 21:58:02 +0800

    计算器

---
+++ h10-15/js/calculate.js
@@ -0,0 +1,17 @@
+var str='';
+var resultNum='';
+function showData(id){
+    str= str+document.getElementById(id).value;
+    document.getElementById("result").innerHTML=str;
+
+}
+function caculate(){
+	resultNum = eval(str);
+    str='';
+	document.getElementById("result").innerHTML=resultNum;
+	str= resultNum;
+}
+function clearData(){
+	document.getElementById("result").innerHTML=0;
+	str='';
+}
